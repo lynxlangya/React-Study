b34ec7c75c097ee9ace74d01a2e0084f53e1df43
王云帆 <wangyunfan@vtstar.net>
2020-03-09 21:42:14 +0800

    day-1: JSX 语法摸索 :rocket:

--- src/index.js
+++ src/index.js
@@ -7,12 +7,20 @@ import ReactDOM from 'react-dom'
 /**创建了名为 Header 的组件, 继承自 Component 类 */
 class Header extends Component {
     render () {
-        const  word = 'is good'
+        const word = 'is good'
+        const isGoodWord = true
         return (
             <div>
                 <h1 style={{'color': 'red'}}>
                     React 小书 {word}
                 </h1>
+                <h2>
+                    React 小书
+                    { isGoodWord 
+                        ? <strong> is good </strong> 
+                        : <span>is not good</span>
+                    }
+                </h2>
             </div>
         )
     }
