413179c82ece34d4a5a69cd49ed662fb0e1bb19e
王云帆 <wangyunfan@vtstar.net>
2020-03-22 10:07:13 +0800

    feat(03-22): React 打卡 :rocket:

--- src/index.tsx
+++ src/index.tsx
@@ -1,67 +1,89 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react'
 
-function createStore(state: any, stateChanger: any) {
+function createStore(stateChanger: any) {
+    let state: any = null
     const listeners:any = []
     const subscribe = (listener: any) => listeners.push(listener)
     const getState = () => state
     const dispatch = (action: any) => {
-        stateChanger(state, action)
+        state = stateChanger(state, action)
         listeners.forEach((listener:any) => listener())
     }
+    dispatch({})
     return { getState, dispatch, subscribe }
 }
 
-function renderApp(appState: any) {
+function renderApp(newAppState: any, oldAppState:any = {}) {
+    if (newAppState === oldAppState) return
     // debugger
     // eslint-disable-next-line no-useless-concat
     console.log('%c' + 'render app', 'color: #2ecc71; font-size: 20px;')
-    renderTitle(appState.title)
-    renderContent(appState.content)
+    renderTitle(newAppState.title, oldAppState.title)
+    renderContent(newAppState.content, oldAppState.content)
 }
 
-function renderTitle(title: any) {
+function renderTitle(newTitle: any, oldTitle = {}) {
+    if (newTitle === oldTitle) return
     // eslint-disable-next-line no-useless-concat
     console.log('%c' + 'render title', 'color: #2ecc71; font-size: 20px;')
     const titleDOM: any = document.getElementById('title')
-    titleDOM.innerHTML = title.text
-    titleDOM.style.color = title.color
+    titleDOM.innerHTML = newTitle.text
+    titleDOM.style.color = newTitle.color
 }
 
-function renderContent(content: any) {
+function renderContent(newContent: any, oldContent = {}) {
+    if (newContent === oldContent) return
     // eslint-disable-next-line no-useless-concat
     console.log('%c' + 'render content', 'color: #2ecc71; font-size: 20px;')
     const contentDOM: any = document.getElementById('content')
-    contentDOM.innerHTML = content.text
-    contentDOM.style.color = content.color
-}
-
-let appState = {
-    title: {
-        text: 'React.js 小书',
-        color: 'red',
-    },
-    content: {
-        text: 'React.js 小书内容',
-        color: 'blue'
-    }
+    contentDOM.innerHTML = newContent.text
+    contentDOM.style.color = newContent.color
 }
 
 function stateChanger(state: any, action: any) {
+    if (!state) {
+        return {
+            title: {
+                text: 'React.js 小书',
+                color: 'red',
+            },
+            content: {
+                text: 'React.js 小书内容',
+                color: 'blue'
+            }
+        }
+    }
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            state.title.text = action.text
-            break
+            return {
+                ...state,
+                title: {
+                    ...state.title,
+                    text: action.text
+                }
+            }
         case 'UPDATE_TITLE_COLOR':
-            state.title.color = action.color
-            break
+            return {
+                ...state,
+                title: {
+                    ...state.title,
+                    color: action.color
+                }
+            }
         default:
-            break
+            return state
     }
 }
 
-const store = createStore(appState, stateChanger)
-store.subscribe(() => renderApp(store.getState())) // 监听数据变化
+const store = createStore(stateChanger)
+/**缓存旧的 state */
+let oldState = store.getState()
+store.subscribe(() => {
+    const newState = store.getState()
+    renderApp(newState, oldState)
+    oldState = newState
+}) // 监听数据变化
 
 renderApp(store.getState()) // 首次渲染页面
 store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
--- src/store.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function createStore (state:any, stateChanger:any):object {
-    const getState = () => state
-    const dispatch = (action: any) => stateChanger(state, action)
-    return {getState, dispatch}
-}
-
-export default createStore
